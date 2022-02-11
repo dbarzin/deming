20981d1860ad61e71771ea642c827a050a89d3e4
dbarzin <dbarzin@gmail.com>
2022-02-11 10:17:34 +0100

    add link on event

--- resources/views/controls/history.blade.php
+++ resources/views/controls/history.blade.php
@@ -246,16 +246,16 @@ Planning des contrôles
             $calendar = new \App\Calendar(\Carbon\Carbon::now()->format('y-m-d'));
             foreach ($controls as $control) {
                 if (($control->score==null) && ($control->plan_date!=null)) {
-                        $calendar->add_event($control->clause, $control->plan_date, 1, 'grey');
+                        $calendar->add_event($control->clause, $control->plan_date, 1, 'grey', $control->id);
                     }
                 else if (($control->score==1) && ($control->realisation_date!=null)) {
-                        $calendar->add_event($control->clause, $control->realisation_date, 1, 'red');
+                        $calendar->add_event($control->clause, $control->realisation_date, 1, 'red', $control->id);
                         }
                 else if (($control->score==2) && ($control->realisation_date!=null)) {
-                        $calendar->add_event($control->clause, $control->realisation_date, 1, 'orange');
+                        $calendar->add_event($control->clause, $control->realisation_date, 1, 'orange', $control->id);
                         }
                 else if (($control->score==3) && ($control->realisation_date!=null)) {
-                        $calendar->add_event($control->clause, $control->realisation_date, 1, 'green');
+                        $calendar->add_event($control->clause, $control->realisation_date, 1, 'green', $control->id);
                     }
                 }
             echo $calendar;
