c581978543fea15329b852cac6766321f6adb649
dbarzin <dbarzin@gmail.com>
2023-04-06 23:46:22 +0200

    fix style

--- resources/views/measures/plan.blade.php
+++ resources/views/measures/plan.blade.php
@@ -42,8 +42,8 @@
 			<div class="cell-1">
 	    		<strong>{{ trans('cruds.measure.fields.periodicity') }}</strong>
 	    	</div>
-			<div class="cell">
-				<select name="periodicity" size="1" width='20'>
+			<div class="cell-2">
+				<select name="periodicity" data-role="select">
 				    <option value="1" {{ $measure->periodicity==1 ? "selected" : ""}}>{{ trans('common.monthly') }}</option>
 				    <option value="3" {{ $measure->periodicity==3 ? "selected" : ""}}>{{ trans('common.quarterly') }}</option>
 				    <option value="6" {{ $measure->periodicity==6 ? "selected" : ""}}>{{ trans('common.biannually') }}</option>
