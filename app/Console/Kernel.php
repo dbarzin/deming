be2218f6f0f9d466804600d27a933cde0a923127
Didier <dbarzin@gmail.com>
2023-06-22 14:07:49 +0200

    add schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,18 +14,19 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\GenerateTestData::class,
+        Commands\SendNotifications::class,
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      *
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('deming:send-notifications')->daily();
     }
 
     /**
