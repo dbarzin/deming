321d8e04ecc25ddd505c79d5724bc14c4a1e04d6
lil-doudou <67382584+lil-doudou@users.noreply.github.com>
2023-07-27 15:11:40 +0200

    Fix: issue fixed by @poooldo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 cd /var/www/deming
@@ -5,4 +6,7 @@ php artisan migrate --seed
 php artisan key:generate
 php artisan storage:link
 php artisan deming:generateTests
-php artisan serve --host 0.0.0.0 --port 8000
+php artisan serve --host 0.0.0.0 --port 8000 &
+
+nginx -g "daemon off;"
+
--- init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE deming CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER 'deming_user'@'%' IDENTIFIED BY 'demPasssword-123';
-GRANT ALL ON deming.* TO deming_user;
-GRANT PROCESS ON *.* TO 'deming_user'@'localhost';
-
-FLUSH PRIVILEGES;
