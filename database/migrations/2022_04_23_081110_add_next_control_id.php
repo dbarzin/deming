d6b81fd52098e68bf4aab87135b379656949a657
Didier <dbarzin@gmail.com>
2023-07-29 23:52:16 +0200

    fix invalid use

--- database/migrations/2022_04_23_081110_add_next_control_id.php
+++ database/migrations/2022_04_23_081110_add_next_control_id.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-use App\Control;
+use App\Models\Control;
 
 class AddNextControlId extends Migration
 {
