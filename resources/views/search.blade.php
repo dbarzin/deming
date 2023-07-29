62cd5ef3acfd5b2014d14f6d527967ac7e0a48e5
Didier <dbarzin@gmail.com>
2023-07-29 22:45:25 +0200

    fix global search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,13 +4,13 @@
 <div class="p-3">
     <div data-role="panel" data-title-caption="Résultats de la recherche" data-collapsible="true" data-title-icon="<span class='mif-search'></span>">
 		@foreach($results as $result)
-		@if ($result['model']==='App\\Domain')
+		@if ($result['model']==='App\\Models\\Domain')
 		   	<b>Domaine</b>
 		   	<a href="/domains/{{ $result['id'] }}">{{ $result['title'] }}</a> : {{ $result['description'] }}
-		@elseif ($result['model']==='App\\Measure')
+		@elseif ($result['model']==='App\\Models\\Measure')
 		   	<b>Mesure</b>
 			<a href="/measures/{{ $result['id'] }}">{{ $result['clause'] }}</a> : {{ $result["name"] }}
-		@elseif ($result['model']==='App\Control')
+		@elseif ($result['model']==='App\\Models\\Control')
 		   	<b>Contrôle</b>
 		   	<a href="/controls/{{ $result['id'] }}">{{ $result["clause"] }}</a> : {{ $result["name"] }}
 			@if (array_key_exists("realisation_date",$result))
