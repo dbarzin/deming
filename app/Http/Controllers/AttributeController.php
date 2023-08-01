2ac402e28b96b8224f2e685d77b3e1c3786177de
Didier <dbarzin@gmail.com>
2023-08-01 17:29:54 +0200

    fix invalid use

--- app/Http/Controllers/AttributeController.php
+++ app/Http/Controllers/AttributeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Attribute;
+use App\Models\Attribute;
 use App\Exports\AttributesExport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
