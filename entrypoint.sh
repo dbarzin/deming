591caba2686a760f5fdc99ebf00a41c8c0d10686
Didier <dbarzin@gmail.com>
2023-08-08 14:16:32 +0200

    update entrypoint to add data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 cd /var/www/deming
-php artisan migrate --seed
+php artisan migrate:fresh --seed
 php artisan key:generate
 php artisan storage:link
+php artisan db:seed --class=AttributeSeeder 
+php artisan db:seed --class=DomainSeeder 
+php artisan db:seed --class=MeasureSeeder
 php artisan deming:generateTests
 php artisan passport:install
 php artisan serve --host 0.0.0.0 --port 8000 &
