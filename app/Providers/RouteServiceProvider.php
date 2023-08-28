9c25b3a1777f0c9ffd770a9ccafd97991bdf581a
ymage <ymage@users.noreply.github.com>
2023-08-28 19:47:19 +0200

    Force https for production

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -23,6 +23,10 @@ class RouteServiceProvider extends ServiceProvider
         //     return Limit::perMinute(60)->by($request->user()?->id ? : $request->ip());
         // });
 
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
