cdb0fa52198349cf40b84e5a2c1c7df935f1ba1d
Didier <dbarzin@gmail.com>
2023-11-02 08:33:16 +0100

    fix route

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -147,14 +147,14 @@
                 </li>
 
                 <li>
-                    <a class="dropdown-item" href="{{ route('logout') }}"
+                    <a class="dropdown-item" href="/logout"
                        onclick="event.preventDefault();
                                      document.getElementById('logout-form').submit();">
                         <span class="icon"><span class="mif-switch"></span></span>
                         <span class="caption">{{ trans("menu.logout") }}</span>
 
                     </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    <form id="logout-form" action="/logout" method="POST" style="display: none;">
                         @csrf
                     </form>
                     </a>
