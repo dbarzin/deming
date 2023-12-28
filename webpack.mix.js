c1f947ade7ef5e501960805eedebbd79d2922c41
Didier <dbarzin@gmail.com>
2023-12-28 13:47:32 +0100

    fix mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,25 +11,26 @@ let mix = require('laravel-mix');
  | file for your application, as well as bundling up your JS files.
  |
  */
+mix
+    .sourceMaps(true, 'source-map')
 
-mix.js([
-        'public/vendors/chartjs/Chart.js',
-        'node_modules/jquery/dist/jquery.min.js',
-        'public/vendors/metro4/js/metro.js',
-        'vendor/enyo/dropzone/dist/dropzone.js',
-        'resources/js/utils.js',
-        'node_modules/easymde/dist/easymde.min.js',
+    .styles([
+            'public/vendors/chartjs/Chart.css',
+            'public/vendors/metro4/css/metro-all.css',
+            'resources/css/app.css',
+            'vendor/enyo/dropzone/dist/dropzone.css',
+            'resources/css/calendar.css',
+            'node_modules/easymde/dist/easymde.min.css',
+            'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css',
         ],
-    'public/js/all.js')
-    .sourceMaps();
+        'public/css/all.css')
 
-mix.styles([
-    'public/vendors/chartjs/Chart.css',
-    'public/vendors/metro4/css/metro-all.css',
-    'resources/css/app.css',
-    'vendor/enyo/dropzone/dist/dropzone.css',
-    'resources/css/calendar.css',
-    'node_modules/easymde/dist/easymde.min.css',
-    'node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css',
-    ],
-    'public/css/all.css');
+    .js([
+            'public/vendors/chartjs/Chart.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'public/vendors/metro4/js/metro.js',
+            'vendor/enyo/dropzone/dist/dropzone.js',
+            'resources/js/utils.js',
+            'node_modules/easymde/dist/easymde.min.js',
+        ],
+        'public/js/all.js');
