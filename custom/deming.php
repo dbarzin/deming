9be4a974efd5681b2c4b1d3cd5e11959657b2e0a
protocolpaladin <protocolpaladin@proton.me>
2024-03-22 14:55:54 +0000

    add option for docker compose prod

---
+++ custom/deming.php
@@ -0,0 +1,10 @@
+<?php return array (
+  'notification' => 
+  array (
+    'mail-from' => 'deming@yourdomain.com',
+    'mail-subject' => '[Deming] - Contrôles',
+    'frequency' => '1',
+    'expire-delay' => '30',
+    'reminder' => NULL,
+  ),
+);
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,15 @@
 cd /var/www/deming
 bash /etc/resetdb.sh
 bash /etc/initialdb.sh
-php artisan key:generate
 php artisan storage:link
 bash /etc/uploadiso27001db.sh
 bash /etc/userdemo.sh
 php artisan passport:install
+php artisan key:generate
+chown www-data:www-data storage/oauth-*.key
+chmod 600 storage/oauth-*.key
 php artisan serve --host 0.0.0.0 --port 8000 &
-
+service postfix start
+service cron start
 nginx -g "daemon off;"
 
