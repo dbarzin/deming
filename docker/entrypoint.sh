470495e5589c595737f2141ca95ba6f2064aca16
didier <dbarzin@gmail.com>
2025-03-02 09:41:24 +0100

    move files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
+cd /var/www/deming
+bash /etc/resetdb.sh
+bash /etc/initialdb.sh
+php artisan storage:link
+bash /etc/uploadiso27001db.sh
+bash /etc/userdemo.sh
+php artisan passport:install
+php artisan key:generate
+chown www-data:www-data storage/oauth-*.key
+chmod 600 storage/oauth-*.key
+php artisan serve --host 0.0.0.0 --port 8000 &
+service postfix start
+service cron start
+nginx -g "daemon off;"
 
-exec "$@"
