6968d3fe64ddd32afdd75b6bfb6e8e27a78e741c
didier <dbarzin@gmail.com>
2025-05-09 19:01:59 +0200

    layout fields

--- resources/views/domains/edit.blade.php
+++ resources/views/domains/edit.blade.php
@@ -1,62 +1,60 @@
 @extends("layout")
 
 @section("content")
-<div class="p-3">
-    <div data-role="panel" data-title-caption="{{ trans('cruds.domain.edit') }}" data-collapsible="true" data-title-icon="<span class='mif-pencil'></span>">
-		@if (count($errors))
+<div data-role="panel" data-title-caption="{{ trans('cruds.domain.edit') }}" data-collapsible="true" data-title-icon="<span class='mif-library'></span>">
+
+@if (count($errors))
+    <div class="remark alert">
+        <span class="mif-report icon"></span>
+			@foreach ($errors->all() as $error)
+                {{ $error }}<br>
+			@endforeach
+    </div>
+@endif
+
+	<form method="POST" action="/domains/{{ $domain->id }}">
+		@method("PATCH")
+		@csrf
 		<div class="grid">
-		    <div class="cell-3 bg-red fg-white">
-				<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-				</ul>
-			</div>
-		</div>
-		@endif
-		<form method="POST" action="/domains/{{ $domain->id }}">
-			@method("PATCH")
-			@csrf
-			<div class="grid">
-		    	<div class="row">
-		    		<div class="cell-1">
-						<label>{{ trans('cruds.domain.fields.framework') }}</label>
-			    	</div>
-					<div class="cell-3">
-						<input type="text" class="input {{ $errors->has('framework') ? 'is-danger' : ''}}" name="framework" value="{{ $errors->has('framework') ?  old('framework') : $domain->framework }}" maxlength='32'>
-					</div>
-				</div>
-		    	<div class="row">
-		    		<div class="cell-1">
-						<label>{{ trans('cruds.domain.fields.name') }}</label>
-			    	</div>
-					<div class="cell-3">
-						<input type="text" class="input {{ $errors->has('title') ? 'is-danger' : ''}}" name="title" value="{{ $errors->has('title') ?  old('title') : $domain->title }}" maxlength='32'>
-					</div>
-				</div>
-		    	<div class="row">
-		    		<div class="cell-1">
-						<label class="label" for="description">{{ trans('cruds.domain.fields.description') }}</label>
-			    	</div>
-					<div class="cell-8">
-						<input type="text" class="input {{ $errors->has('description') ? 'is-danger' : ''}}" name="description" value="{{ $errors->has('description') ?  old('description') : $domain->description }}" maxlength='255'>
-					</div>
+	    	<div class="row">
+	    		<div class="cell-1">
+					<label>{{ trans('cruds.domain.fields.framework') }}</label>
+		    	</div>
+				<div class="cell-3">
+					<input type="text" class="input {{ $errors->has('framework') ? 'is-danger' : ''}}" name="framework" value="{{ $errors->has('framework') ?  old('framework') : $domain->framework }}" maxlength='32'>
 				</div>
 			</div>
-			<button type="submit" class="button success">
-	            <span class="mif-floppy-disk"></span>
-				&nbsp;
-				{{ trans('common.save') }}
-			</button>
-		</form>
-        &nbsp;
-		<form action="/domains/{{ $domain->id }}">
-			<button type="submit" class="button">
-				<span class="mif-cancel"></span>
-				&nbsp;
-    			{{ trans('common.cancel') }}
-    		</button>
-		</form>
-	</div>
+	    	<div class="row">
+	    		<div class="cell-1">
+					<label>{{ trans('cruds.domain.fields.name') }}</label>
+		    	</div>
+				<div class="cell-3">
+					<input type="text" class="input {{ $errors->has('title') ? 'is-danger' : ''}}" name="title" value="{{ $errors->has('title') ?  old('title') : $domain->title }}" maxlength='32'>
+				</div>
+			</div>
+	    	<div class="row">
+	    		<div class="cell-1">
+					<label class="label" for="description">{{ trans('cruds.domain.fields.description') }}</label>
+		    	</div>
+				<div class="cell-8">
+					<input type="text" class="input {{ $errors->has('description') ? 'is-danger' : ''}}" name="description" value="{{ $errors->has('description') ?  old('description') : $domain->description }}" maxlength='255'>
+				</div>
+			</div>
+        	<div class="row">
+                <div class="cell-6">
+            		<button type="submit" class="button success">
+                        <span class="mif-floppy-disk2"></span>
+            			&nbsp;
+            			{{ trans('common.save') }}
+            		</button>
+                    <a href="/domains/{{ $domain->id }}" class="button">
+                		<span class="mif-cancel"></span>
+                		&nbsp;
+                		{{ trans('common.cancel') }}
+                    </a>
+                </div>
+            </div>
+        </div>
+	</form>
 </div>
 @endsection
--- resources/views/domains/show.blade.php
+++ resources/views/domains/show.blade.php
@@ -1,82 +1,76 @@
 @extends("layout")
 
 @section("content")
-<div class="p-3">
-    <div data-role="panel" data-title-caption="{{ trans('cruds.domain.show') }}" data-collapsible="true" data-title-icon="<span class='mif-library'></span>">
-		@if (count($errors))
-		<div class="grid">
-		    <div class="cell-5 bg-red fg-white">
-				<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-				</ul>
-			</div>
+<div data-role="panel" data-title-caption="{{ trans('cruds.domain.show') }}" data-collapsible="true" data-title-icon="<span class='mif-library'></span>">
+	@if (count($errors))
+	<div class="grid">
+	    <div class="cell-5 bg-red fg-white">
+			<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+			</ul>
 		</div>
-		@endif
-		<div class="grid">
-	    	<div class="row">
-	    		<div class="cell-1">
-		    		<strong>{{ trans('cruds.domain.fields.framework') }}</strong>
-		    	</div>
-	    		<div class="cell">
-		    		{{ $domain->framework }}
-	    		</div>
-		    </div>
-
-	    	<div class="row">
-	    		<div class="cell-1">
-		    		<strong>{{ trans('cruds.domain.fields.name') }}</strong>
-		    	</div>
-	    		<div class="cell">
-		    		{{ $domain->title }}
-	    		</div>
-		    </div>
-
-	    	<div class="row">
-	    		<div class="cell-1">
-		    		<strong>{{ trans('cruds.domain.fields.description') }}</strong>
-		    	</div>
-	    		<div class="cell">
-		    		{{ $domain->description }}
-	    		</div>
+	</div>
+	@endif
+	<div class="grid">
+    	<div class="row">
+    		<div class="cell-1">
+	    		<strong>{{ trans('cruds.domain.fields.framework') }}</strong>
 	    	</div>
+            <div class="cell-6">
+	    		{{ $domain->framework }}
+    		</div>
 	    </div>
 
     	<div class="row">
     		<div class="cell-1">
+	    		<strong>{{ trans('cruds.domain.fields.name') }}</strong>
+	    	</div>
+            <div class="cell-6">
+	    		{{ $domain->title }}
+    		</div>
+	    </div>
+
+    	<div class="row">
+    		<div class="cell-1">
+	    		<strong>{{ trans('cruds.domain.fields.description') }}</strong>
+	    	</div>
+            <div class="cell-6">
+	    		{{ $domain->description }}
     		</div>
     	</div>
+    </div>
 
-		<div class="form-group">
-			@if (Auth::User()->role==1)
-		    <form action="/domains/{{ $domain->id }}/edit">
-		    	<button class="button primary">
-		            <span class="mif-wrench"></span>
-		            &nbsp;
-		    		{{ trans('common.edit') }}
-		    	</button>
-		        &nbsp;
-		    </form>
-			<form action="/domains/{{ $domain->id }}" method="post" onSubmit="if(!confirm('{{ trans('common.confirm') }}')){return false;}">
-	           {{ method_field('delete') }}
-	           @csrf
-		        <button class="button alert" type="submit">
-					<span class="mif-fire"></span>
-					&nbsp;
-			        {{ trans('common.delete') }}
-		    	</button>
-        	</form>
-			&nbsp;
-			@endif
-			<form action="/domains">
-		    	<button class="button">
+	<div class="row">
+		<div class="cell-1">
+		</div>
+	</div>
+
+	<div class="row">
+        <div class="cell-6">
+    			@if (Auth::User()->role==1)
+                <a href="/domains/{{ $domain->id }}/edit" class="button primary">
+    		            <span class="mif-wrench"></span>
+    		            &nbsp;
+    		    		{{ trans('common.edit') }}
+                </a>
+    			<form action="/domains/{{ $domain->id }}" class="d-inline" method="post" onSubmit="if(!confirm('{{ trans('common.confirm') }}')){return false;}">
+    	           {{ method_field('delete') }}
+    	           @csrf
+    		        <button class="button alert" type="submit">
+    					<span class="mif-fire"></span>
+    					&nbsp;
+    			        {{ trans('common.delete') }}
+    		    	</button>
+            	</form>
+    			@endif
+                <a href="/domains" class="button">
 					<span class="mif-cancel"></span>
 					&nbsp;
 		    		{{ trans('common.cancel') }}
-		    	</button>
-			</form>
-		</div>
-	</div>
+                </a>
+    	</div>
+    </div>
 </div>
 @endsection
