e7e1861207c0593c47f7538838ae49e9e072cbfb
didier <dbarzin@gmail.com>
2025-06-13 14:36:33 +0200

    add initials on page

--- app/Models/User.php
+++ app/Models/User.php
@@ -61,4 +61,21 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Control::class)->whereNull('realisation_date')->orderBy('name');
     }
+
+    public function initiales(): string {
+        // On supprime les espaces en trop en début/fin et on remplace les multiples espaces internes par un seul
+        $nom = trim(preg_replace('/\s+/', ' ', $this->name));
+
+        // On découpe la chaîne en mots
+        $mots = explode(' ', $nom);
+
+        if (count($mots) === 1) {
+            // Un seul mot : on renvoie les deux premières lettres
+            return substr($mots[0], 0, 2);
+        } else {
+            // Deux mots ou plus : on renvoie la première lettre du premier et du deuxième mot
+            return substr($mots[0], 0, 1) . substr($mots[1], 0, 1);
+        }
+    }
+
 }
