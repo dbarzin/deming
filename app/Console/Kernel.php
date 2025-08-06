bb4bf7db2b867ff6556a646c9aaa8bcf7fea90d3
didier <dbarzin@gmail.com>
2025-08-06 08:49:33 +0200

    add cleanup command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\GenerateTestData::class,
         Commands\SendNotifications::class,
+        Commands\Cleanup::class,
     ];
 
     /**
