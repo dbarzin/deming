46c8631984b2a7173292cf11552f5489da3e0dd8
dbarzin <dbarzin@gmail.com>
2026-02-01 15:15:48 +0100

    add link count

--- resources/views/documents/check.blade.php
+++ resources/views/documents/check.blade.php
@@ -11,10 +11,24 @@
         <th>{{ trans('cruds.document.fields.name') }}</th>
         <th>{{ trans('cruds.document.fields.size') }}</th>
         <th>{{ trans('cruds.document.fields.hash') }}</th>
+        <th>Links</th>
+        <th>Status</th>
     </tr>
     </thead>
 
     @foreach ($documents as $doc)
+    @php
+        $filePath = storage_path('docs/' . $doc->id);
+        $fileExists = file_exists($filePath);
+        $linkCount = 0;
+        $hashValid = false;
+
+        if ($fileExists) {
+            $stats = stat($filePath);
+            $linkCount = $stats['nlink'] ?? 0;
+            $hashValid = ($doc->hash === hash_file('sha256', $filePath));
+        }
+    @endphp
     <tr>
         <td>
             {{ $doc->id }}
@@ -23,25 +37,35 @@
             <a href="/bob/show/{{ $doc->control_id }}">{{ $doc->control_id }}</a>
         </td>
         <td>
-            <a href="/doc/show/{{ $doc->id }}">{{ substr($doc->filename,0,32) }}</a>
+            <a href="/doc/show/{{ $doc->id }}">{{ substr($doc->filename, 0, 32) }}</a>
         </td>
         <td>
             {{ \Illuminate\Support\Number::fileSize($doc->size) }}
         </td>
         <td>
-            {{ $doc->hash }}
-            <br>
+            <small>{{ $doc->hash }}</small>
+        </td>
+        <td class="text-center">
+            @if ($fileExists)
+                @if ($linkCount > 1)
+                    <span class="badge bg-blue">{{ $linkCount }}</span>
+                @else
+                    {{ $linkCount }}
+                @endif
+            @else
+                -
+            @endif
         </td>
         <td>
             <b>
-            @if (file_exists(storage_path('docs/').$doc->id))
-                @if ($doc->hash == hash_file("sha256", storage_path('docs/').$doc->id))
-                    <font color="green">OK</font>
+            @if ($fileExists)
+                @if ($hashValid)
+                    <span style="color: green; ">OK</span>
                 @else
-                    <font color="red">HASH FAILS</font>
+                    <span style="color: red">HASH FAILS</span>
                 @endif
             @else
-                    <font color="red">MISSING</font>
+                <span style="color: red">MISSING</span>
             @endif
             </b>
         </td>
